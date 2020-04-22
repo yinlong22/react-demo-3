473d15c14f839d7d904fed641e0eb9caf4b42c16
yinlong22 <342241857@.com>
2020-04-22 20:19:14 +0800

    react组件

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 class App extends React.PureComponent {
-    constructor(props) {
+    divRef=undefined;//好习惯，把特殊属性写在前面，声明自己要创动态的divRef
+    constructor(props) {//初始化各类数据，对象和函数
         super(props);
-        this.state = {n: 1, m: 1,array:[1,2,3,4,5,6]}
+        this.state = {n: 1, m: 1,array:[1,2,3,4,5,6],width:undefined};
+        this.divRef = React.createRef()//
     }
 
     onClick = () => {
@@ -17,6 +19,17 @@ class App extends React.PureComponent {
     //     return newState.n !== this.state.n;
     // }    / 如果值不变，会自动匹配到值不变，不更新传入B的UI，这样做节省了2个更新步骤。React.PureComponent内置了这个功能
 
+    //插入页面的时期
+    componentDidMount() {//首次渲染会执行此钩子，最好在这里写获取宽高和ajax请求代码
+        // 获取屏宽 ↓ ↓
+        // const div = document.getElementById('xxx') /获取div的id
+        // const {width} = div.getBoundingClientRect() /获取屏宽
+        // this.setState({width}) /这种写法是DOM的 react ↓ ↓
+        const div = this.divRef.current //引用当前divRef的方法
+        const {width} = div.getBoundingClientRect()
+        this.setState({width})
+    }
+
     render() {
         console.log('render了一次')
         // let result = [] /for循环没有返回值，我们要给他一个结果
@@ -26,7 +39,8 @@ class App extends React.PureComponent {
         return (
             // 由于只能有一个根元素，所以为了方便用Fragment来占位。可简写为<>!!!
             <React.Fragment>
-                <div>hi</div>
+                <div ref={this.divRef}>hi , 获取的屏宽为{this.state.width}px</div>
+                {/*<div id="xxx">hi , 获取屏宽为:{this.state.width}px</div> /DOM写法获取屏宽 react ↑ ↑*/}
                 <div className="App">
                     m:{this.state.m}
                     {this.state.n % 2 === 0 ?
@@ -51,7 +65,7 @@ class B extends React.Component {
 
     render() {
         return (//读出传入的外部数据this.state.x
-            <div>n:{this.props.name}</div>//UI在B里刷新 /值不变且直接跳过了刷新
+            <div>n:{this.props.name}</div>//UI在B里刷新 /值不变便直接跳过了刷新
         )
     }
 }
